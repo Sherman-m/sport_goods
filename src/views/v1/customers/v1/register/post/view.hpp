194714e16d2895e2952fa8fa2b3f99f2d955e1ec
Sherman-m <Sherman-88@yandex.ru>
2024-12-19 22:31:48 +0000

    redesign structure

---
+++ src/views/v1/customers/v1/register/post/view.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <userver/components/component_list.hpp>
+
+namespace sport_goods::v1::customers::post {
+
+void AppendV1CustomerV1RegisterPost(
+    userver::components::ComponentList& component_list);
+
+}
