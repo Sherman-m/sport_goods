f429549638cbd04f147f9522ccdc2ad24bce8f7d
Sherman-m <Sherman-88@yandex.ru>
2024-12-27 01:31:38 +0300

    add handler for order creation

---
+++ backend/src/views/v1/orders/post/view.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <userver/components/component_list.hpp>
+
+namespace sport_goods::views::v1::orders::post {
+
+void AppendView(userver::components::ComponentList& component_list);
+
+}  // namespace sport_goods::views::v1::orders::post
--- backend/src/views/v1/products/get/view.hpp
+++ backend/src/views/v1/products/get/view.hpp
@@ -2,7 +2,7 @@
 
 #include <userver/components/component_list.hpp>
 
-namespace sport_goods::v1::orders::get {
+namespace sport_goods::v1::products::get {
 
 void View(userver::components::ComponentList& component_list);
 }
