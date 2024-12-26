32d7871e80ab7b1cc3af92a9b91b817fbf96c390
Sherman-m <Sherman-88@yandex.ru>
2024-12-27 01:30:27 +0300

    redesigned db: remove customers, refactored orders for landing page

--- backend/postgresql/schemas/db_1.sql
+++ backend/postgresql/schemas/db_1.sql
@@ -2,12 +2,12 @@ DROP SCHEMA IF EXISTS sport_goods CASCADE;
 
 CREATE SCHEMA IF NOT EXISTS sport_goods;
 
-CREATE TABLE IF NOT EXISTS sport_goods.customers (
+CREATE TABLE IF NOT EXISTS sport_goods.orders (
     id serial PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    date TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS sport_goods.categories (
@@ -21,19 +21,5 @@ CREATE TABLE IF NOT EXISTS sport_goods.products (
     description TEXT,
     category_id INTEGER NOT NULL REFERENCES sport_goods.categories (id),
     price DECIMAL(10, 2) NOT NULL,
-    quantity INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS sport_goods.orders (
-  id serial PRIMARY KEY,
-  customers_id INTEGER NOT NULL REFERENCES sport_goods.customers (id),
-  date TIMESTAMPTZ NOT NULL DEFAULT now(),
-  status TEXT NOT NULL,
-  total_amount DECIMAL(10, 2) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS sport_goods.order_items (
-    product_id INTEGER NOT NULL REFERENCES sport_goods.products (id),
-    order_id INTEGER NOT NULL REFERENCES sport_goods.orders (id),
     quantity INTEGER NOT NULL
 );
